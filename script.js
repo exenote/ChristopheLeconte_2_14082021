97739eebf7db4f44dfce71eba2ac6c119d1bdcf0
exenote <46305957+exenote@users.noreply.github.com>
2021-11-18 11:53:00 +0100

    h1 >> h2, a >> div, footer

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function check() {
-  var e1 = document.getElementById("id1");
-  var wi = window.getComputedStyle(e1, null).getPropertyValue("width");
-  document.getElementById("check").innerHTML = wi;
-}
+// function check() {
+//   var e1 = document.getElementById("id1");
+//   var wi = window.getComputedStyle(e1, null).getPropertyValue("width");
+//   document.getElementById("check").innerHTML = wi;
+// }
 
-window.onresize = check;
+// window.onresize = check;
